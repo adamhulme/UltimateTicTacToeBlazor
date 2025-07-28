7a84d2a5b9a35398c57d26fc0e6f24997069ed09
Adam Hulme <Adam.Hulme+cai@coxautoinc.com>
2025-07-28 20:07:15 +0100

    Add game modes with time limits

---
+++ UltimateTicTacToeBlazor/Models/GameMode.cs
@@ -0,0 +1,9 @@
+namespace UltimateTicTacToe.Models;
+
+public enum GameMode
+{
+    Unlimited,
+    Blitz,      // 30 seconds per move
+    Rapid,      // 60 seconds per move
+    Classical   // 3 minutes per move
+}
