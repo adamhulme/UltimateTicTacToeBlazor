9acc3b4522a1dcf7bf0f51877c149f3288436ade
Adam Hulme <Adam.Hulme+cai@coxautoinc.com>
2025-11-30 22:18:26 +0000

    Add option to play with time increments and custom (user-set) time/increment

--- UltimateTicTacToeBlazor/Models/GameMode.cs
+++ UltimateTicTacToeBlazor/Models/GameMode.cs
@@ -5,8 +5,9 @@ public enum GameMode
     Unlimited,
     Blitz,      // 30 seconds per move
     Rapid,      // 60 seconds per move
-    Classical,  // 3 minutes per move
     Chess5,     // 5 minutes per player total
     Chess10,    // 10 minutes per player total
-    Chess15     // 15 minutes per player total
+    Chess5Inc3, // 5 minutes + 3 seconds increment
+    Chess10Inc5,// 10 minutes + 5 seconds increment
+    Custom      // Custom total time and increment
 }
